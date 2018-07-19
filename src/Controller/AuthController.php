157a9d9334c030007c6e9a606f5d676bd4e9f6cb
jeremy <jeremymartinsio1@gmail.com>
2018-07-19 16:29:43 +0200

    update User.php

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -35,8 +35,4 @@ class AuthController extends AbstractController
     {
         return new Response(sprintf('Logged in as %s', $this->getUser()->getUsername()));
     }
-
-    public function login() {
-
-    }
 }
